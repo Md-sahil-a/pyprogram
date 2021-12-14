d523f4326baea1cddfc9b9f31cee5d77ab18a4b9
Md sahil amaan <77974484+sahilamaan@users.noreply.github.com>
2021-12-14 15:23:30 +0530

    pytHON

---
+++ pythonPrograms/Multiplication.py
@@ -0,0 +1,5 @@
+
+my_number = int(input("Enter the number which multipication you want:  "))
+print("Multiplication of ", my_number)
+for i in range(1, 11):
+  print(my_number ,'x' , i, "=", i* my_number )
