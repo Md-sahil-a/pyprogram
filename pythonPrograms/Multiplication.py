906ec63e54b86e9a000f17b6f08874050af2f313
Md Sahil Amaan <77974484+sahilamaan@users.noreply.github.com>
2021-12-14 15:30:21 +0530

    Update Multiplication.py

--- pythonPrograms/Multiplication.py
+++ pythonPrograms/Multiplication.py
@@ -1,5 +1,5 @@
 
-my_number = int(input("Enter the number which multipication you want:  "))
+my_number = int(input("Enter the table number you want to learn:  "))
 print("Multiplication of ", my_number)
 for i in range(1, 11):
   print(my_number ,'x' , i, "=", i* my_number )
